45fa86513b100121c879bc4f8a6f5ea48d19c14d
Aleksei Piatachkov <pyatachkov.ao@gmail.com>
2023-03-23 18:19:02 +0500

    Added new folder Example006 CondisonIfElse

---
+++ Example006_CondisonIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.Write("Ура, это же Маша!");
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
