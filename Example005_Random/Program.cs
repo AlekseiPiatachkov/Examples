a819707804582f5fef02948eb8cd87448d057198
Aleksei Piatachkov <pyatachkov.ao@gmail.com>
2023-03-23 18:02:53 +0500

    Added new folder Example005 Randome

---
+++ Example005_Random/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = new Random().Next(1, 10); // 1, 2, 3. 4 ....9
+Console.Write(numberA);
+Console.Write("+");
+int numberB = new Random().Next(1, 10); // 1, 2, 3. 4 ....9
+Console.Write(numberB);
+Console.Write("=");
+int result = numberA + numberB;
+Console.WriteLine(result);
